4525db5d6b51a3864577d8b70a5af798efa9573f
Rafael Shen <rafael.shen977@gmail.com>
2019-09-18 22:33:58 +0800

    Modal with variable content etc

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -3,10 +3,21 @@ import React from "react";
 import Modal from "../../modal";
 
 export default () => {
+  const renderActions = (
+    <React.Fragment>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
+
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure to delete this stream?"
+        actions={renderActions}
+      />
     </div>
   );
 };
--- streams/client/src/modal.js
+++ streams/client/src/modal.js
@@ -7,8 +7,9 @@ export default props => {
   return ReactDOM.createPortal(
     <div className="ui active dimmer" onClick={() => history.push("/")}>
       <div className="ui active modal" onClick={e => e.stopPropagation()}>
-        <div className="ui header">This is modal header</div>
-        <div className="content">Modal content</div>
+        <div className="ui header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector("#modal")
