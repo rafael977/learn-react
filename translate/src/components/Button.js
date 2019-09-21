28235b1447166e99d1e21b2845e45ab9e552af62
Rafael Shen <rafael.shen977@gmail.com>
2019-09-21 23:56:00 +0800

    [translate] Multi context

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,17 +1,28 @@
 import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
+
+const renderSubmit = lan => {
+  return lan === "english" ? "Submit" : "提交";
+};
+
+const renderButton = color => {
+  console.log(color);
+
+  return (
+    <button className={`ui button ${color}`}>
+      <LanguageContext.Consumer>
+        {lan => renderSubmit(lan)}
+      </LanguageContext.Consumer>
+    </button>
+  );
+};
 
 const Button = () => {
   return (
-    <LanguageContext.Consumer>
-      {value =>
-        value === "english" ? (
-          <button className="ui button primary">Submit</button>
-        ) : (
-          <button className="ui button primary">提交</button>
-        )
-      }
-    </LanguageContext.Consumer>
+    <ColorContext.Consumer>
+      {color => renderButton(color)}
+    </ColorContext.Consumer>
   );
 };
 
