bef88a47de499d94955d421c116f8f768f35c56b
Rafael Shen <rafael.shen977@gmail.com>
2019-09-21 16:23:38 +0800

    Put language selector

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,8 +1,30 @@
 import React from "react";
 
-class App extends React.Component{
+class App extends React.Component {
+  state = {
+    language: "english"
+  };
+
+  onLanguageSelected = language => {
+    this.setState({ language });
+  };
+
   render() {
-    return <div>App</div>
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageSelected("english")}
+          />
+          <i
+            className="flag cn"
+            onClick={() => this.onLanguageSelected("chinese")}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
